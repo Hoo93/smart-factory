cb4a5f72314765091b77a1a3fbef0bddd2c618a3
hoo93 <sksk8922@gmail.com>
2024-08-10 23:55:15 +0900

    feat: instanceGenerator constructor 구현

--- factory/factory.interface.ts
+++ factory/factory.interface.ts
@@ -6,11 +6,13 @@ interface PrimaryKeyOption {
   name: string;
   type: 'string' | 'number';
 }
+
 class Factory {
   private primaryIdSet: Set<any>;
   public tableName: string;
+  public instanceGenerator;
 
-  constructor(tableName: string, createFunction: {}, config: FactoryOption) {
+  constructor(tableName: string, instanceGenerator: Function, config: FactoryOption) {
     if (config.primaryKey.type === 'string') {
       this.primaryIdSet = new Set<string>();
     } else if (config.primaryKey.type === 'number') {
@@ -19,5 +21,6 @@ class Factory {
       throw new Error('Invalid primary key type');
     }
     this.tableName = tableName;
+    this.instanceGenerator = instanceGenerator;
   }
 }
