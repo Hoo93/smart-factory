1632dcff57fa9debaef6fbe3c75077b18f4b2a66
hoo93 <sksk8922@gmail.com>
2024-08-11 00:05:55 +0900

    feat: FactoryManager 함수 제네릭 적용

--- factory/factory-manager.ts
+++ factory/factory-manager.ts
@@ -1,3 +1,6 @@
-class FactoryManager {
-  
-}
+import { FactoryOption, InstanceGeneratorFunction } from './types';
+import { Factory } from './factory.interface';
+
+export const defineFactory = <T>(tableName: string, instanceGenerator: InstanceGeneratorFunction<T>, config: FactoryOption) => {
+  return new Factory(tableName, instanceGenerator, config);
+};
