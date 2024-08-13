c201973b2018cf58251ce6b1c4a69fcc73483114
Hoo93 <sksk8922@gmail.com>
2024-08-14 01:25:18 +0900

    feat: FactoryManager Class defineFactory 에서 존재 여부 검증

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,23 @@
-import { defineFactorifyConfig } from './config'
-import { FactoryModel } from './model'
-import type { Builder } from './builder/builder'
-import type { DefineFactoryCallback } from './contracts'
+import { defineFactorifyConfig } from './config';
+import { FactoryModel } from './model';
+import type { Builder } from './builder/builder';
+import type { DefineFactoryCallback } from './contracts';
+import { FactoryManager } from './factory-manager';
 
-export { defineFactorifyConfig, FactoryModel, Builder }
+export { defineFactorifyConfig, FactoryModel, Builder };
 
 /**
  * Define a new factory.
  */
-export function defineFactory<T extends Record<string, any>>(
-  table: string,
-  cb: DefineFactoryCallback<T>
-) {
-  return new FactoryModel<T>(table, cb)
+export function defineFactory<T extends Record<string, any>>(table: string, cb: DefineFactoryCallback<T>) {
+  const tableNameManager = FactoryManager.getInstance();
+
+  if (tableNameManager.hasTableName(table)) {
+    throw new Error(`Table name "${table}" already exists.`);
+  }
+
+  // Register the table name in the singleton manager
+  tableNameManager.addFactory(table);
+
+  return new FactoryModel<T>(table, cb);
 }
