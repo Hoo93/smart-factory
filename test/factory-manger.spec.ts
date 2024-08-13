17626f38361d0326c9ae69a6287101ce87596095
Hoo93 <sksk8922@gmail.com>
2024-08-14 01:25:40 +0900

    feat: 중복 테이블 명이 존재하는 경우 에러 발생 테스트코드 구현

--- test/factory-manger.spec.ts
+++ test/factory-manger.spec.ts
@@ -1,39 +1,24 @@
-import { FactoryOption, InstanceGeneratorFunction } from '../factory/types';
-import { defineFactory } from '../factory/factory-manager';
-import { Factory } from '../factory/factory.interface';
+import { defineFactory } from '../src/index';
 
 describe('Factory Manager Test', () => {
   interface TestInterface {
     id: string;
     name: string;
-    email: string;
   }
 
-  it('should create a Factory instance with the correct parameters', () => {
+  it('if already has tableName, than should occur error', () => {
     // Given
-    const tableName = 'users';
-
-    const instanceGenerator: InstanceGeneratorFunction<TestInterface> = () => {
+    const testCallBack = () => () => {
       return {
-        id: 'test id',
-        name: 'test name',
-        email: 'test@email.com',
+        id: 'TEST ID',
+        name: 'TEST NAME',
       };
     };
-    const config: FactoryOption = {
-      primaryKey: {
-        name: 'id',
-        type: 'string',
-      },
-    };
 
-    // When
-    const factory = defineFactory(tableName, instanceGenerator, config);
-
-    // Then
-    expect(factory).toBeInstanceOf(Factory); // Check that factory is an instance of Factory
-    expect(factory.tableName).toBe(tableName); // Check that tableName is set correctly
-    expect(factory.instanceGenerator).toBe(instanceGenerator); // Check that instanceGenerator is set correctly
-    expect(factory['primaryIdSet']).toBeInstanceOf(Set); // Check that primaryIdSet is a Set
+    // When, Then
+    expect(() => {
+      const UserFactory = defineFactory<TestInterface>('users', testCallBack());
+      const AnotherUserFactory = defineFactory<TestInterface>('users', testCallBack());
+    }).toThrow(new Error(`Table name "users" already exists.`));
   });
 });
