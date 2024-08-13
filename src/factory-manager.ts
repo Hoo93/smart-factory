d072bbe33fc7b50d3f3c139315ecb504a35a5b72
Hoo93 <sksk8922@gmail.com>
2024-08-14 01:25:00 +0900

    feat: 전체 테이블 명을 관리하는 FactoryManager Class 구현

---
+++ src/factory-manager.ts
@@ -0,0 +1,32 @@
+// Singleton class to manage table names
+export class FactoryManager {
+  private static instance: FactoryManager;
+  private tableNames: Set<string>;
+
+  private constructor() {
+    this.tableNames = new Set<string>();
+  }
+
+  public static getInstance(): FactoryManager {
+    if (!FactoryManager.instance) {
+      console.log('getInstance has been called');
+      FactoryManager.instance = new FactoryManager();
+    }
+    return FactoryManager.instance;
+  }
+
+  public addFactory(tableName: string) {
+    if (this.tableNames.has(tableName)) {
+      throw new Error(`Table name "${tableName}" already exists.`);
+    }
+    this.tableNames.add(tableName);
+  }
+
+  public hasTableName(tableName: string): boolean {
+    return this.tableNames.has(tableName);
+  }
+
+  public removeTableName(tableName: string) {
+    this.tableNames.delete(tableName);
+  }
+}
