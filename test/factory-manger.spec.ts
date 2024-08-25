d4fb5d2f16f8d1ef565ceb24e184dcd40f1b9f78
Hoo93 <sksk8922@gmail.com>
2024-08-25 20:27:14 +0900

    test: add addRelation test

--- test/factory-manger.spec.ts
+++ test/factory-manger.spec.ts
@@ -1,16 +1,31 @@
 import { defineFactory } from '../src/index';
+import { FactoryManager } from '../src/manager/factory_manager';
+
+interface UserInterface {
+  id: number;
+  name: string;
+}
+
+interface PostInterface {
+  id: number;
+  title: string;
+  content: string;
+  userId: number;
+}
+
+interface CommentInterface {
+  id: number;
+  content: string;
+  postId: number;
+  userId: number;
+}
 
 describe('Factory Manager Test', () => {
-  interface TestInterface {
-    id: string;
-    name: string;
-  }
-
   it('if already has tableName, than should occur error', () => {
     // Given
-    const testCallBack = () => () => {
+    const testCallBack = () => {
       return {
-        id: 'TEST ID',
+        id: 1,
         name: 'TEST NAME',
       };
     };
@@ -18,8 +33,68 @@ describe('Factory Manager Test', () => {
     // When, Then
     // tableName 으로 unique 한 factory 를 생성한다.
     expect(() => {
-      const UserFactory = defineFactory<TestInterface>('users', testCallBack());
-      const AnotherUserFactory = defineFactory<TestInterface>('users', testCallBack());
+      const UserFactory = defineFactory<UserInterface>('users', testCallBack);
+      const AnotherUserFactory = defineFactory<UserInterface>('users', testCallBack);
     }).toThrow(new Error(`Table name "users" already exists.`));
+  });
+
+  describe('addRelationTest', () => {
+    it('hasMany Relation create belongs to opposite', () => {
+      // Given
+
+      const UserFactory = defineFactory<UserInterface>('users', () => {
+        return {
+          id: 1,
+          name: 'TEST NAME',
+        };
+      });
+      const PostFactory = defineFactory<PostInterface>('posts', () => {
+        return {
+          id: 1,
+          title: 'TEST TITLE',
+          content: 'TEST CONTENT',
+          userId: 1,
+        };
+      });
+
+      // When
+      UserFactory.hasMany('posts');
+
+      const userRelations = FactoryManager.getInstance().getTableRelation('users');
+      const postRelations = FactoryManager.getInstance().getTableRelation('posts');
+
+      // Then
+      expect(userRelations?.['has-many'].has('posts')).toBeTruthy();
+      expect(postRelations?.['belongs-to'].has('users')).toBeTruthy();
+    });
+
+    it('hasOne Relation create belongs to opposite', () => {
+      // Given
+
+      const UserFactory = defineFactory<UserInterface>('users', () => {
+        return {
+          id: 1,
+          name: 'TEST NAME',
+        };
+      });
+      const PostFactory = defineFactory<PostInterface>('posts', () => {
+        return {
+          id: 1,
+          title: 'TEST TITLE',
+          content: 'TEST CONTENT',
+          userId: 1,
+        };
+      });
+
+      // When
+      UserFactory.hasOne('posts');
+
+      const userRelations = FactoryManager.getInstance().getTableRelation('users');
+      const postRelations = FactoryManager.getInstance().getTableRelation('posts');
+
+      // Then
+      expect(userRelations?.['has-one'].has('posts')).toBeTruthy();
+      expect(postRelations?.['belongs-to'].has('users')).toBeTruthy();
+    });
   });
 });
